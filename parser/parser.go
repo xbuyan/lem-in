ace265195104114f9495226348afd7b7d18aec17
xbuyan <kevobuyan@gmail.com>
2026-04-13 12:14:07 +0300

    complete parser with validation

--- parser/parser.go
+++ parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	"lem-in/graph"
@@ -21,8 +22,8 @@ func ParseFile(filename string) (*graph.Colony, error) {
 	scanner := bufio.NewScanner(file)
 	nextIsStart := false
 	nextIsEnd := false
-	_ = nextIsStart
-	_ = nextIsEnd
+	antCountRead := false
+
 	for scanner.Scan() {
 		// remove leading spaces
 		line := scanner.Text()
@@ -42,7 +43,68 @@ func ParseFile(filename string) (*graph.Colony, error) {
 		if strings.HasPrefix(line, "#") {
 			continue
 		}
+		if !antCountRead {
 
+			count, err := strconv.Atoi(line)
+
+			if err != nil || count <= 0 {
+				return nil, fmt.Errorf("Error: invalid data format, invalid number of ants")
+			}
+			colony.NumAnts = count
+			antCountRead = true
+			continue
+		}
+
+		if strings.Contains(line, "-") {
+			parts := strings.Split(line, "-")
+
+			if len(parts) != 2 {
+				return nil, fmt.Errorf("ERROR: invalid data format, invalid tunnel")
+			}
+			room1, exists1 := colony.Rooms[parts[0]]
+			room2, exists2 := colony.Rooms[parts[1]]
+
+			if !exists1 || !exists2 {
+				return nil, fmt.Errorf("Error: invalid data format, tunnel to unknown room")
+			}
+			room1.Connections = append(room1.Connections, room2)
+			room2.Connections = append(room2.Connections, room1)
+			continue
+		}
+		// handle room definitions
+		parts := strings.Fields(line)
+		if len(parts) == 3 {
+			x, errX := strconv.Atoi(parts[1])
+			y, errY := strconv.Atoi(parts[2])
+			if errX != nil || errY != nil {
+				return nil, fmt.Errorf("ERROR: invalid data format, invalid room coordinates")
+			}
+			room := &graph.Room{
+				Name: parts[0],
+				X:    x,
+				Y:    y,
+			}
+			if nextIsStart {
+				colony.Start = room
+				nextIsStart = false
+			}
+			if nextIsEnd {
+				colony.End = room
+				nextIsEnd = false
+			}
+			colony.Rooms[room.Name] = room
+			continue
+		}
+
+	}
+	if colony.Start == nil {
+		return nil, fmt.Errorf("Error: invalid data format, no start room found")
+	}
+	if colony.End == nil {
+		return nil, fmt.Errorf("Error: invalid data format, no end room found")
+	}
+	if colony.NumAnts == 0 {
+		return nil, fmt.Errorf("Error: invalid data format, invalid numer of ants")
 	}
 	return colony, nil
 }
