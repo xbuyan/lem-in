99af7e064aaf127f19052d78f2c0b0931a174a39
xbuyan <kevobuyan@gmail.com>
2026-04-13 13:17:22 +0300

    wire parser to main and verify with test0.txt

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"lem-in/parser"
+)
 
 func main() {
-	fmt.Println("lem-in starting...")
+	if len(os.Args) != 2 {
+
+		fmt.Println("ERROR: invalid usage, expected: go run . <filename>")
+		os.Exit(1)
+	}
+	colony, err := parser.ParseFile(os.Args[1])
+	if err != nil {
+
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("Ants: %d\n", colony.NumAnts)
+	fmt.Printf("Rooms: %d\n", len(colony.Rooms))
+	fmt.Printf("Start: %s\n", colony.Start.Name)
+	fmt.Printf("End: %s\n", colony.End.Name)
 }
