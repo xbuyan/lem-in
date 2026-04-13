f472fc62a34c4fca9acfc9eec1643ac86e447414
xbuyan <kevobuyan@gmail.com>
2026-04-13 10:51:20 +0300

    add ParseFile function with file opening and colony initialization

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,20 @@
 package parser
+
+import (
+	"fmt"
+	"os"
+
+	"lem-in/graph"
+)
+
+func ParseFile(filename string) (*graph.Colony, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("Error: invalid data format, could not open file")
+	}
+	defer file.Close()
+	colony := &graph.Colony{
+		Rooms: make(map[string]*graph.Room),
+	}
+	return colony, nil
+}
