14672016c15902740ba78e4bb7b45de38a25c598
xbuyan <kevobuyan@gmail.com>
2026-04-14 11:06:04 +0300

    add reconstructPath function to solver

--- solver/solver.go
+++ solver/solver.go
@@ -1,1 +1,21 @@
 package solver
+
+import "lem-in/graph"
+
+func reconstructPath(previous map[string]*graph.Room, start *graph.Room, end *graph.Room) []*graph.Room {
+	var path []*graph.Room
+	current := end
+
+	for current != start {
+		path = append(path, current)
+		current = previous[current.Name]
+	}
+
+	path = append(path, start)
+
+	for i, j := 0, len(path)-1; i < j; i, j = i+1, j-1 {
+		path[i], path[j] = path[j], path[i]
+	}
+
+	return path
+}
