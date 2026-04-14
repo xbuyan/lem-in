e22e369680d6c3bdafe19aae3f3dd9c9447e1fd2
xbuyan <kevobuyan@gmail.com>
2026-04-14 11:35:35 +0300

    wire BFS solver to main and verify shortest path

--- main.go
+++ main.go
@@ -5,22 +5,31 @@ import (
 	"os"
 
 	"lem-in/parser"
+	"lem-in/solver"
 )
 
 func main() {
 	if len(os.Args) != 2 {
-
 		fmt.Println("ERROR: invalid usage, expected: go run . <filename>")
 		os.Exit(1)
 	}
+
 	colony, err := parser.ParseFile(os.Args[1])
 	if err != nil {
-
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	path := solver.BFS(colony)
+	if path == nil {
+		fmt.Println("ERROR: no path found")
+		os.Exit(1)
+	}
+
 	fmt.Printf("Ants: %d\n", colony.NumAnts)
-	fmt.Printf("Rooms: %d\n", len(colony.Rooms))
-	fmt.Printf("Start: %s\n", colony.Start.Name)
-	fmt.Printf("End: %s\n", colony.End.Name)
+	fmt.Printf("Path found: ")
+	for _, room := range path {
+		fmt.Printf("%s ", room.Name)
+	}
+	fmt.Println()
 }
